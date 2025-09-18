7eb4752c5a5b4cd1864fbaca1188c9341141ad6f
Serious-Fin <augustasgri@gmail.com>
2025-09-18 18:05:19 +0300

    fix getting shares

--- api/shares/share.go
+++ api/shares/share.go
@@ -2,6 +2,7 @@ package shares
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 
 	"github.com/google/uuid"
@@ -11,8 +12,15 @@ import (
 type Share struct {
 	Content  string `json:"content"`
 	Title    string `json:"title,omitempty"`
-	ExpireIn string `json:"expireIn,omitempty"`
+	ExpireAt string `json:"expireAt,omitempty"`
 	Password string `json:"password,omitempty"`
+}
+
+type DBShare struct {
+	Content  string
+	Title    sql.NullString
+	ExpireAt sql.NullString
+	Password sql.NullString
 }
 
 type CreateShareResponse struct {
@@ -29,7 +37,7 @@ func NewShareHandler(db *pgx.Conn) *ShareDBHandler {
 
 func (handler *ShareDBHandler) CreateShare(request Share) (*CreateShareResponse, error) {
 	id := uuid.NewString()
-	_, err := handler.DB.Exec(context.Background(), "INSERT INTO shares (id, title, content, password, expireat) VALUES ($1, $2, $3, $4, $5);", id, request.Title, request.Content, request.Password, request.ExpireIn)
+	_, err := handler.DB.Exec(context.Background(), "INSERT INTO shares (id, title, content, password, expireat) VALUES ($1, $2, $3, $4, $5);", id, request.Title, request.Content, request.Password, request.ExpireAt)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't create new share: %w", err)
 	}
@@ -37,10 +45,35 @@ func (handler *ShareDBHandler) CreateShare(request Share) (*CreateShareResponse,
 }
 
 func (handler *ShareDBHandler) GetShare(id string) (*Share, error) {
-	var share Share
-	err := handler.DB.QueryRow(context.Background(), "SELECT content, title, expire_at, password FROM shares WHERE id = $1;", id).Scan(&share.Content, &share.Title, &share.ExpireIn, &share.Password)
+	var dbShare DBShare
+	err := handler.DB.QueryRow(context.Background(), "SELECT title, content, expire_at, password FROM shares WHERE id = $1;", id).Scan(&dbShare.Title, &dbShare.Content, &dbShare.ExpireAt, &dbShare.Password)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't find share with id '%s': %w", id, err)
 	}
+
+	share := GetShareFromDBObject(dbShare)
 	return &share, nil
+}
+
+func GetShareFromDBObject(dbShare DBShare) Share {
+	var share Share
+	share.Content = dbShare.Content
+	if dbShare.Title.Valid {
+		share.Title = dbShare.Title.String
+	} else {
+		share.Title = ""
+	}
+
+	if dbShare.ExpireAt.Valid {
+		share.ExpireAt = dbShare.ExpireAt.String
+	} else {
+		share.ExpireAt = ""
+	}
+
+	if dbShare.Password.Valid {
+		share.Password = dbShare.Password.String
+	} else {
+		share.Password = ""
+	}
+	return share
 }
