451429113994ce7ac9dfdd42e8838ccb2c1a14bb
Serious-Fin <augustasgri@gmail.com>
2025-11-23 18:07:02 +0200

    added separate launch params for dev server

--- dev.sh
+++ dev.sh
@@ -12,11 +12,11 @@ then
 
     # API window
     tmux new-window -t $session:1 -n 'api'
-    tmux send-keys -t 'api' 'cd api' C-m 'go run .' C-m
+    tmux send-keys -t 'api' 'cd api' C-m 'DATABASE_URL=postgres://postgres:root@localhost:5432/qr_pastebin go run .' C-m
 
     # Web window
     tmux new-window -t $session:2 -n 'web'
-    tmux send-keys -t 'web' 'cd web' C-m 'npm run dev' C-m
+    tmux send-keys -t 'web' 'cd web' C-m 'npm run developmentserver' C-m
 fi
 
 tmux attach-session -t $session:0
